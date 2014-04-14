d2e7ae8f54234e33ca8efe027605813603cb4fc9
Ariel Nunez <ingenieroariel@gmail.com>
2014-04-14 15:30:09 +0200

    No geoserver

--- geonode/settings.py
+++ geonode/settings.py
@@ -397,35 +397,10 @@ CACHE_TIME=0
 
 # OGC (WMS/WFS/WCS) Server Settings
 OGC_SERVER = {
-    'default' : {
-        'BACKEND' : 'geonode.geoserver',
-        'LOCATION' : 'http://localhost:8080/geoserver/',
-        # PUBLIC_LOCATION needs to be kept like this because in dev mode
-        # the proxy won't work and the integration tests will fail
-        # the entire block has to be overridden in the local_settings
-        'PUBLIC_LOCATION' : 'http://localhost:8080/geoserver/',
-        'USER' : 'admin',
-        'PASSWORD' : 'geoserver',
-        'MAPFISH_PRINT_ENABLED' : True,
-        'PRINTNG_ENABLED' : True,
-        'GEONODE_SECURITY_ENABLED' : True,
-        'GEOGIT_ENABLED' : False,
-        'WMST_ENABLED' : False,
-        'BACKEND_WRITE_ENABLED': True,
-        'WPS_ENABLED' : True,
-        # Set to name of database in DATABASES dictionary to enable
-        'DATASTORE': '', #'datastore',
-        'TIMEOUT': 10  # number of seconds to allow for HTTP requests
-    }
 }
 
 # Uploader Settings
 UPLOADER = {
-    'BACKEND' : 'geonode.rest',
-    'OPTIONS' : {
-        'TIME_ENABLED': False,
-        'GEOGIT_ENABLED': False,
-    }
 }
 
 # CSW settings
